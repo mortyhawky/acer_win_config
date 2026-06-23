bc85580278631938c5fc6f2c62e2387e7d959e84
mortyhawky <morty.hawky@outlook.com>
2026-06-23 04:19:54 +0200

    Tue Jun 23 04:19:54 WEST 2026

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.number			= true
+vim.opt.relativenumber		= true
+
+vim.opt.background		= dark
+vim.cmd.colorscheme		('habamax')
+
+vim.g.mapleader			= " "
+vim.keymap.set	( "n", "<leader>so", ":source<CR>" )
