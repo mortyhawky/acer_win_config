271b832bfae2fd137aadfc8b99d127d7647f3713
mortyhawky <morty.hawky@outlook.com>
2026-06-29 05:48:43 +0200

    Mon Jun 29 05:48:42 WEST 2026

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,8 @@
 vim.opt.background           = dark
 vim.cmd.colorscheme          ('habamax')
 
-vim.opt.number               = true
-vim.opt.relativenumber       = true
+--vim.opt.number               = true
+--vim.opt.relativenumber       = true
 
 vim.opt.tabstop              = 4
 vim.opt.shiftwidth           = 4
@@ -13,15 +13,19 @@ vim.opt.cursorcolumn         = true
 vim.opt.guicursor:append     ("i:hor20")
 
 vim.g.mapleader			     = " "
-vim.keymap.set	( "n",       "<leader>ex", ":Explore<CR>" )
 vim.keymap.set	( {"n","i"}, "<C-s>",      "<Esc>:nohls<CR>:write<CR>"   )
+vim.keymap.set	( "n",       "<leader>ex", ":Explore<CR>" )
 vim.keymap.set	( "n",       "<leader>bn", ":bn<CR>" )
 vim.keymap.set	( "n",       "<leader>bp", ":bp<CR>" )
+vim.keymap.set	( "n",       "<leader>no", ":set nonumber<CR>:set norelativenumber<CR>" )
+vim.keymap.set	( "n",       "<leader>nr", ":set number<CR>:set relativenumber<CR>" )
 
-vim.keymap.set  ( "n",       "<C-h>",      ":echo 'C-h'<CR>"   )
+--vim.keymap.set  ( "n",       "<C-h>",      ":echo 'C-h'<CR>"   )
 
 -- Ctrl+h,j are remapped in alacritty to F13 and F14
 vim.keymap.set  ( "n",       "<F13>",      ":wincmd h<CR>"   )
+vim.keymap.set  ( "n",       "<C-h>",      ":wincmd h<CR>"   )
 vim.keymap.set  ( "n",       "<F14>",      ":wincmd j<CR>"   )
+vim.keymap.set  ( "n",       "<C-j>",      ":wincmd j<CR>"   )
 vim.keymap.set  ( "n",       "<C-k>",      ":wincmd k<CR>"   )
 vim.keymap.set  ( "n",       "<C-l>",      ":wincmd l<CR>"   )
--- scoop/README.md
+++ scoop/README.md
@@ -3,5 +3,6 @@
 export:
 ```bash
 scoop export > $Env:USERPROFILE\.config\scoop\installed_apps.json
-scoop export > $Env:XDG_CONFIG_HOME\scoop\installed_apps.json
+scoop export > $Env:XDG_CONFIG_HOME\scoop\installed_apps-$(Get-Date -Format 'yyyy-MM-dd_HHmm').json
+scoop list   > "$Env:XDG_CONFIG_HOME\scoop\list-$(Get-Date -Format 'yyyy-MM-dd_HHmm')"
 ```
